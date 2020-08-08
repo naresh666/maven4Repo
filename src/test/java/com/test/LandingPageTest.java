15151c42c1146cb27d05f06d9fd0e976475aa0af
naresh666 <nareshan23@gmail.com>
2020-08-08 23:08:32 +0530

    1st commit

---
+++ src/test/java/com/test/LandingPageTest.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LandingPageTest {
+	@Test
+	public void tc1() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void tc2() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void tc3() {
+		Assert.assertTrue(true);
+	}
+
+}
---
+++ src/test/java/com/test/SignInPageTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SignInPageTest {
+	@Test
+	public void tc3() {
+		Assert.assertTrue(true);
+	}
+
+}
