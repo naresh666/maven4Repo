ea408be68a23ff8e9b3ed65ba44b6566fde12f34
naresh666 <nareshan23@gmail.com>
2020-08-09 08:44:38 +0530

    added some.xml, sanity.xml file for jenkins parameres practice purpose and modified pom.xml file addd global variables and pass valus dynamically to the pom.xml file

--- src/test/java/com/test/LandingPageTest.java
+++ src/test/java/com/test/LandingPageTest.java
@@ -6,16 +6,23 @@ import org.testng.annotations.Test;
 public class LandingPageTest {
 	@Test
 	public void tc1() {
+		System.out.println("browser name >>>>>>>>>>>>>>>>"+System.getProperty("browser"));
+		System.out.println("url is >>>>>>>>>>>>>>>>>>>"+System.getProperty("url"));
 		Assert.assertTrue(true);
 	}
 	
 	@Test
 	public void tc2() {
+		System.out.println("browser name >>>>>>>>>>>>>>>>"+System.getProperty("browser"));
+		System.out.println("url is >>>>>>>>>>>>>>>>>>>"+System.getProperty("url"));
 		Assert.assertTrue(true);
+		
 	}
 	
 	@Test
 	public void tc3() {
+		System.out.println("browser name >>>>>>>>>>>>>>>>"+System.getProperty("browser"));
+		System.out.println("url is >>>>>>>>>>>>>>>>>>>"+System.getProperty("url"));
 		Assert.assertTrue(true);
 	}
 
--- src/test/java/com/test/SignInPageTest.java
+++ src/test/java/com/test/SignInPageTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 public class SignInPageTest {
 	@Test
 	public void tc3() {
+		System.out.println("browser name >>>>>>>>>>>>>>>>"+System.getProperty("browser"));
+		System.out.println("url is >>>>>>>>>>>>>>>>>>>"+System.getProperty("url"));
 		Assert.assertTrue(true);
 	}
 
